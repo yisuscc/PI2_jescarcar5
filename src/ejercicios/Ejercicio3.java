0727400fdb6d456e9a7200081ccbf47b6a60e4fd
jesus <jesus@jesus-PROX15>
2022-11-10 10:53:30 +0100

    ejercicio 3 hecho sin los tests de complejidad

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -3,18 +3,22 @@ package ejercicios;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
+import us.lsi.common.Files2;
 import us.lsi.tiposrecursivos.BinaryTree;
 import us.lsi.tiposrecursivos.BinaryTree.BEmpty;
 import us.lsi.tiposrecursivos.BinaryTree.BLeaf;
 import us.lsi.tiposrecursivos.BinaryTree.BTree;
+import us.lsi.tiposrecursivos.BinaryTrees;
 import us.lsi.tiposrecursivos.Tree;
 import us.lsi.tiposrecursivos.Tree.TEmpty;
 import us.lsi.tiposrecursivos.Tree.TLeaf;
 import us.lsi.tiposrecursivos.Tree.TNary;
+import us.lsi.tiposrecursivos.parsers.BinaryTreeParser;
 
 public class Ejercicio3 {
-	public static List<String>caminoSinCaracterNArio(Tree<Character> arbol, Character K){
+	public static List<String>caminosSinCaracterNArio(Tree<Character> arbol, Character K){
 		List<String> res = new ArrayList<>();
 		casoRecursivo(arbol, K, "", res);
 		return res;
@@ -32,16 +36,16 @@ public class Ejercicio3 {
 
 //			caso hoja: comprobamos que si etiqueta no sea la letrea y slo mentemos en la cadena y la cadena en la lista
 		case TLeaf<Character> t:
-			if (t.label().equals(K)) {
-				ac = ac + K;
+			if (!t.label().equals(K)) {
+				ac = ac + t.label();
 				res.add(ac);
 			}
 			;
 			break;
 
 		case TNary<Character> t:
-			if (t.label().equals(K)) {
-				ac = ac + K;
+			if (!t.label().equals(K)) {
+				ac = ac + t.label();
 //			con stream no sale :(
 				for (Tree<Character> a : t.elements()) {
 					casoRecursivo(a, K, ac, res);
@@ -55,7 +59,7 @@ public class Ejercicio3 {
 		
 
 	}
-	public static List<String>caminoSinCaracterNArio(BinaryTree<Character> arbol, Character K){
+	public static List<String>caminosSinCaracterBinario(BinaryTree<Character> arbol, Character K){
 		List<String> res = new ArrayList<>();
 		casoRecursivoBT(arbol, K, "", res);
 		return res;
@@ -64,15 +68,15 @@ public class Ejercicio3 {
 	private static void casoRecursivoBT(BinaryTree<Character> arbol, Character K, String ac, List<String> res) {
 		switch (arbol) {
 		case BEmpty<Character> t: break;
-		case BLeaf<Character> t : if (t.label().equals(K)) {
-			ac = ac + K;
+		case BLeaf<Character> t : if (!t.label().equals(K)) {
+			ac = ac + t.label();
 			res.add(ac);
 		};
 		break;
 		
 		case BTree<Character> t : 
-			if (t.label().equals(K)) {
-				ac = ac + K;
+			if (!t.label().equals(K)) {
+				ac = ac + t.label();
 				casoRecursivoBT(t.left(), K, ac, res);
 				casoRecursivoBT(t.right(), K, ac, res);
 				
@@ -80,11 +84,49 @@ public class Ejercicio3 {
 			break;
 		default: break;
 		}
+		/*
+		 * private static void cargaDatos() {
+	Consumer<String> cnsmr = x -> {
+		//String[] y = x.split("#");
+		BinaryTree<Integer> bt =  BinaryTreeImpl.parse(x,s-> Integer.parseInt(s));
+		//Pair<BinaryTree<Integer>,Integer> p = Pair.of(bt, Integer.parseInt(y[1]));
+		System.out.println(bt);
+		System.out.println(recursivo(bt));
+	};
+	Files2.streamFromFile("ficheros/PI3E5_DatosEntrada.txt").forEach(cnsmr);
+}
+		 * 
+		 * */
+		
+		 
 
+	}
+	private static void cargaDatosBT() {
+		Consumer<String> cnsmr = x -> {
+			String[] y = x.split("#");
+			BinaryTree<Character> bt =  BinaryTree.parse(y[0],  s-> s.charAt(0));
+			
+			System.out.println("Arbol: "+ bt);
+			System.out.println("Caracter: " +y[1]);
+			System.out.println(caminosSinCaracterBinario(bt, y[1].charAt(0)));
+		};
+		Files2.streamFromFile("ficheros/Ejercicio3DatosEntradaBinario.txt").forEach(cnsmr);
+	}
+	private static void cargaDatosNario() {
+		Consumer<String> cnsmr = x -> {
+			String[] y = x.split("#");
+			Tree<Character> bt =  Tree.parse(y[0],  s-> s.charAt(0));
+			
+			System.out.println("Arbol: "+bt);
+			System.out.println("Caracter: " +y[1]);
+			System.out.println(caminosSinCaracterNArio(bt, y[1].charAt(0)));
+		};
+		Files2.streamFromFile("ficheros/Ejercicio3DatosEntradaBinario.txt").forEach(cnsmr);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		cargaDatosBT();
+		cargaDatosNario();
 
 	}
 
