d8b4356491d9105601aaba287de08e5f95132212
jesus <jesus@jesus-PROX15>
2022-11-10 13:07:13 +0100

    la version n aria no termina de funcionar

--- src/ejercicios/Ejercicio4.java
+++ src/ejercicios/Ejercicio4.java
@@ -4,10 +4,15 @@ import java.util.function.Consumer;
 
 import us.lsi.common.Files2;
 import us.lsi.common.String2;
+import us.lsi.streams.Stream2;
 import us.lsi.tiposrecursivos.BinaryTree;
 import us.lsi.tiposrecursivos.BinaryTree.BEmpty;
 import us.lsi.tiposrecursivos.BinaryTree.BLeaf;
 import us.lsi.tiposrecursivos.BinaryTree.BTree;
+import us.lsi.tiposrecursivos.Tree;
+import us.lsi.tiposrecursivos.Tree.TEmpty;
+import us.lsi.tiposrecursivos.Tree.TLeaf;
+import us.lsi.tiposrecursivos.Tree.TNary;
 
 public class Ejercicio4 {
 	
@@ -39,20 +44,47 @@ public class Ejercicio4 {
 		return res;
 		
 	}
-	private static Integer nVocalesBT(BinaryTree<String> bt) {
-		 Integer res = null;
-		switch (bt) {
-		case BEmpty<String> t: res =0; //devuelve 0
-		case BLeaf<String> t: res = cuentavocales(t.label());
-		break;
-		case BTree<String> t: // comprueba sus vocales y devuelvelas sulyas mas la sum de sus hijos
-			res = cuentavocales(t.label()) +nVocalesBT(t.left()) +nVocalesBT(t.right());
-		break;
-		}
+	public static Boolean ej4Nario (Tree<String> nt) {
+		return casoRecursivoNario(nt, true);
 		
-		return ac;
+	}
+	private static Boolean casoRecursivoNario(Tree<String> nt, Boolean res) {
+		
+		switch(nt) {
+		case TEmpty<String> t: break; // rompemos directamente
+		case TLeaf<String> t: break; // rompemos directamente
+		case TNary<String> t: 
+
+		//probar con stream2.allequals 
+			if(t.elements().stream().map(e->cuentavocales(e.optionalLabel().
+					orElseGet(()-> ""))).distinct().count()==1) {
+				res = Stream2.allEquals(t.elements().stream().map(a-> casoRecursivoNario(a, true)));
+			}
+			else res = false;
+			
+			break; // solo se comparan los hijos
+			
 		}
+		return res;
+		
+	}
 	
+	
+	
+//	private static Integer nVocalesBT(BinaryTree<String> bt) {
+//		 Integer res = null;
+//		switch (bt) {
+//		case BEmpty<String> t: res =0; //devuelve 0
+//		case BLeaf<String> t: res = cuentavocales(t.label());
+//		break;
+//		case BTree<String> t: // comprueba sus vocales y devuelvelas sulyas mas la sum de sus hijos
+//			res = cuentavocales(t.label()) +nVocalesBT(t.left()) +nVocalesBT(t.right());
+//		break;
+//		}
+//		
+//		return ac;
+//		}
+//	
 	
 	
 	
@@ -89,8 +121,21 @@ public class Ejercicio4 {
 		};
 		Files2.streamFromFile("ficheros/Ejercicio4DatosEntradaBinario.txt").forEach(cnsmr);
 	}
+	
+	private static void cargaDatosNario() {
+		Consumer<String> cnsmr = x -> {
+			
+			Tree<String> bt =  Tree.parse(x);
+			
+			System.out.println("Arbol: "+bt);
+			
+			System.out.println(ej4Nario(bt));
+		};
+		Files2.streamFromFile("ficheros/Ejercicio4DatosEntradaNario.txt").forEach(cnsmr);
+	}
 	public static void main(String[] args) {
-		cargaDatosBT();		
+		//cargaDatosBT();	
+		cargaDatosNario();
 
 	}
 
